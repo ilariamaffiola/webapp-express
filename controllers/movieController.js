c181f17132aed8a2f8f27dc9e028b5a52f9730c2
Ilaria <maffiolailaria1@gmail.com>
2025-05-29 17:05:28 +0200

    [UPD] Update index function in movieController to handle database query and error responses

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,7 +3,13 @@ const connection = require('../data/db');
 
 //index
 const index = (req, res) => {
-    res.json('mi sto esaurendo quindi pusherò');
+    connection.query("SELECT * FROM movie", (err, movieResults) => {
+        if(err){
+                res.status(500).json({error: "Database query failed:" +err });
+            }
+
+        res.json(movieResults);
+    })
 }
 
 const show = (req, res) => {
